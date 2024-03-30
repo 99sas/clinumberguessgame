457deaab31dd9bfb104c9116a47ec5c9ac6af0f5
99sas <164977732+99sas@users.noreply.github.com>
2024-03-31 01:22:42 +0500

    Add files via upload

---
+++ guess.ts
@@ -0,0 +1,21 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+
+let randomNumber= Math.floor(Math.random()*6 + 1)
+
+const answer= await inquirer.prompt([{
+    name: "number",
+    type: "number",
+    message: "Guess the number",
+}])
+if (answer.Number===randomNumber)
+{
+    console.log("Congratulations! you won the game")
+}
+else
+ { 
+    console.log("Sorry! your guess is wrong")
+
+ }
